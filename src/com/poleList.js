0dbce30a7fc7f494dd7807f27dd3cc9f03c0ac40
성민 <tjdals14798@naver.com>
2022-05-16 22:47:32 +0900

    22-02-16 전주번호 별 그래프 완성

--- src/com/poleList.js
+++ src/com/poleList.js
@@ -7,6 +7,7 @@ Chart.register(...registerables);
 
 export default function PoleList({index,onKeyDown,ck,modal}){
     const inputRef = useRef();
+    const [idx,setIdx] = useState('');
 
     useEffect(()=>{
         inputRef.current.focus();
@@ -19,14 +20,14 @@ export default function PoleList({index,onKeyDown,ck,modal}){
     //     poleTilt:[],
     // })
 
-    const onModal = (pole) =>{
-        modal(true);
-        console.log(pole.poleNum);
+    const onModal = (e) =>{
+        setIdx(e.target.value)
+        modal(true)
     }
-
+    
     return(
         <div className="polelist">
-            { ck &&<PoleChart modal={modal}/> }
+            { ck && <PoleChart modal={modal} idx={idx} index={index}/> }
             <input ref={inputRef} className="search" placeholder="search" onKeyDown={onKeyDown}></input>
             <table>
                 <tbody>
@@ -36,9 +37,9 @@ export default function PoleList({index,onKeyDown,ck,modal}){
 
                     {index.map((pole,i) => (
                         <tr key={i}>
-                            <td >{pole.poleNum}</td>
-                            <td >{pole.poleAdmin}</td>
-                            <td ><button className="chartbtn" onClick={onModal} > chart </button></td>
+                            <td>{pole.poleNum}</td>
+                            <td>{pole.poleAdmin}</td>
+                            <td><button className="chartbtn" value={i} onClick={onModal} > chart </button></td>
                         </tr>
                     ))}
                 </tbody>
@@ -47,17 +48,16 @@ export default function PoleList({index,onKeyDown,ck,modal}){
     );
 }
 
-function PoleChart({modal}){
-      
+function PoleChart({modal,index,idx}){
     const data = {
-        labels: ['January', 'February', 'March', 'April', 'May'],
+        labels: index[idx].poleDate,
         datasets: [
           {
             type: 'line',
-            label: 'Dataset 1',
+            label: "기울기",
             borderColor: 'rgb(54, 162, 235)',
-            borderWidth: 2,
-            data: [1, 2, 3, 4, 5],
+            borderWidth:2,
+            data: index[idx].poleTilt,
           }
         ],
       };
@@ -66,7 +66,7 @@ function PoleChart({modal}){
       <div className="modalBackground">
         <div className="modalContainer">
         <div className="titleCloseBtn"><button onClick={() => modal(false)}> X </button></div>
-              <div className="title"><h1>~번의 그래프</h1></div>
+              <div className="title"><h1>전주 번호 : {index[idx].poleNum}</h1></div>
               <Line className="body" type="line" data={data} />
         </div>
       </div>
