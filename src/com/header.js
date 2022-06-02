6bae6210d2d2af86d355bb3fa7874e9298b2d7e4
성민 <tjdals14798@naver.com>
2022-06-02 20:34:59 +0900

    22-06-02 타이머

--- src/com/header.js
+++ src/com/header.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "../Css/App.css";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const HederTimer = styled.div`
+    position: absolute;
+    width: 250px;
+    left: 50%;
+    margin-left: -125px;
+`;
 
 export default function Header({loginState}){
 
     const Navigate = useNavigate();
+    const today = new Date();
+    const dateString = today.toLocaleDateString('ko-KR',{
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+    const timeString = today.toLocaleTimeString('ko-KR',{
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+    });
+
 
     const move = () =>{
         Navigate("/login")
@@ -17,6 +37,7 @@ export default function Header({loginState}){
     return(
         <header className="header">
             <button onClick={home} className="headerButton"> Pole Of Pisa </button>
+            <HederTimer>{dateString} : {timeString}</HederTimer>
             <button onClick={move} className="headerLogin"> {loginState} </button>
         </header>
     );
