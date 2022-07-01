0256c5f0010afc30b1c5a85dcf900ca012157145
성민 <tjdals14798@naver.com>
2022-07-01 20:49:36 +0900

    22-07-01 poleInfo

--- src/com/poleInfo.js
+++ src/com/poleInfo.js
@@ -1,10 +1,13 @@
 /* global kakao */
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import index from "./pole";
-import "../Css/Modal.css"
+import Header from "./header";
+import Footer from "./footer";
+import { Form, Container, Table, Nav } from 'react-bootstrap';
+import { useLocation } from "react-router-dom";
 import styled, { css } from 'styled-components';
 import {MdDone, MdDelete} from 'react-icons/md'
-import { useLocation, useNavigate } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function PoleInfo(){  
     useEffect(()=>{
@@ -20,11 +23,10 @@ export default function PoleInfo(){
       });
       marker.setMap(map);
     }, []);
-
+    
     const {state} = useLocation();
-    const Navigate = useNavigate();
-
     const { kakao } = window;
+    
 
     const [listTodos,SetListTodos] = useState([
       {
@@ -72,67 +74,80 @@ export default function PoleInfo(){
 
     return(
     <>
-    <div className="modalBackground">
-      <div className="modalContainer">
-        <div className="titleCloseBtn"><button onClick={() => { Navigate("/") } }> X </button></div>
-              <div className="title"><h1>전주 번호 : {index[state].poleNum}</h1></div>
-              <table className="infoTable">
-                <tbody>
-                    <tr>
-                        <td>최종변동 일</td><td>{index[state].poleDate[4]}</td>
-                    </tr>
-                    <tr>
-                        <td>현재 기울기</td><td>{index[state].poleTilt[4]}</td>
-                    </tr>
-                    <tr>
-                        <td>관리자</td><td>{index[state].poleAdmin}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <div className="title"><h1>할 일</h1></div>
-
-            <div className="todoList">
-            {listTodos.map(todo => (
-              <div key={todo.id}>
-              <TodoItemBlock>
-                <CheckCircle onClick={() => onToggle(todo.id)} done={todo.done}>{todo.done && <MdDone />}</CheckCircle>
-                <Text done={todo.done}>{todo.text}</Text>
-                  <Remove onClick={()=>{onRemove(todo.id)}}>
-                    <MdDelete />
-                  </Remove>
-              </TodoItemBlock>
-              </div>
+      <Header />
+      <Container fluid='xl' className="mt-4">
+      <Table striped bordered hover variant="dark" className="m-auto w-75">
+          <thead>
+            <tr>
+              <th style={thStyle} colSpan={2}>전주번호 : {index[state].poleNum}</th>
+            </tr>
+          </thead>
+        <tbody>
+          <tr>
+            <td>관리자</td>
+            <td>{index[state].poleAdmin}</td>
+          </tr>
+          <tr>
+            <td>현재 기울기</td>
+            <td>{index[state].poleTilt[4]}</td>
+          </tr>
+          <tr>
+            <td>최종변동 일</td>
+            <td>{index[state].poleDate[4]}</td>
+          </tr>
+        </tbody>
+      </Table>
+      <Table striped bordered hover variant="dark" className="m-auto mt-4 w-75">
+          <thead>
+            <tr>
+              <th style={thStyle}>할 일</th>
+            </tr>
+          </thead>
+        <tbody>
+        {listTodos.map(todo => (
+              <tr key={todo.id}>
+                <td>
+                  <Nav className="align-items-center p-1">
+                    <CheckCircle onClick={() => onToggle(todo.id)} done={todo.done}>{todo.done && <MdDone />}</CheckCircle>
+                    <Text done={todo.done}>{todo.text}</Text>
+                    <Remove onClick={()=>{onRemove(todo.id)}}><MdDelete/></Remove>
+                  </Nav>
+                </td>
+              </tr>
             ))}
-              <InsertForm onSubmit={onCreate}>
-                <Input autoFocus placeholder="할 일을 입력 후, Enter 를 누르세요" value={inputs} onChange={onChange}/>
-              </InsertForm>
-              </div>
-
-            <div className="title"><h1>전주 위치</h1></div>
-            <div className="poleMap">
-        	    <div id="map" style={{width:"80%", height:"30vh"}}></div> 
-            </div>
-        </div>
-    </div>
-    </>  
+            <tr>
+              <td>
+                <Form onSubmit={onCreate}>
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>할 일 추가</Form.Label>
+                    <Form.Control type="text" autoFocus placeholder="할 일을 입력 후, Enter 를 누르세요" value={inputs} onChange={onChange}/>
+                  </Form.Group>
+                </Form>
+              </td>
+            </tr>
+        </tbody>
+      </Table>
+      <Table striped bordered hover variant="dark" className="m-auto mt-4 w-75">
+        <thead>
+          <tr>
+            <th style={thStyle}>전주 위치</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th id="map" style={{width:"60%", height:"30vh"}}></th>
+          </tr>
+        </tbody>
+      </Table>
+      </Container>  
+    <Footer />
+    </>
     )
 }
 
-const Input = styled.input`
-  padding: 12px;
-  border-radius: 4px;
-  border: 1px solid #dee2e6;
-  width: 100%;
-  outline: none;
-  font-size: 18px;
-  box-sizing: border-box;
-`;
-
-const InsertForm = styled.form`
-  border-bottom-left-radius: 16px;
-  border-bottom-right-radius: 16px;
-  border-top: 1px solid #e9ecef;
-`;
+const thStyle={
+  fontSize:"24px"
+}
 
 const Remove = styled.div`
   display: flex;
@@ -146,19 +161,12 @@ const Remove = styled.div`
   }
 `;
 
-const TodoItemBlock = styled.div`
-  display: flex;
-  align-items: center;
-  padding-top: 12px;
-  padding-bottom: 12px;
-`;
-
 const CheckCircle = styled.div`
-  width: 32px;
-  height: 32px;
-  border-radius: 16px;
+  width: 28px;
+  height: 28px;
+  border-radius: 13px;
   border: 1px solid #ced4da;
-  font-size: 24px;
+  font-size: 20px;
   display: flex;
   align-items: center;
   justify-content: center;
